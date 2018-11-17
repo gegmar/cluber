4d34b993fd0f7734fa0c25df0a437bb3f61f4ed0
Martin Gegenleitner <martin@gegenleitner.eu>
2018-11-18 00:21:01 +0100

    Fixed a bug in EventFactory StartController returns only current projects and events

---
+++ app/Http/Controllers/StartController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+use App\Event;
+
+class StartController extends Controller
+{
+    /**
+     * Show the starting page of the ticket shop.
+     * 
+     * Works for both guests and authenticated users.
+     * 
+     * Displays all current projects and events.
+     */
+    public function index()
+    {
+        $projects = Project::with(['events' => function ($query) {
+            $query->where('start_date', '>=', new \DateTime());
+        }])->get();
+
+        $currentProjects = $projects->filter(function ($project) {
+            return $project->events->count() > 0;
+        })->all();
+
+        return view('start', ['projects' => $currentProjects]);
+    }
+}
--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -4,9 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Event::class, function (Faker $faker) {
     $dateTime = new DateTime();
+    $startDate = clone $dateTime->add(new DateInterval('PT' . random_int(0, 525600) . 'M')); // 525600 = Sum of minutes of a year
+    $endDate = $dateTime->add(new DateInterval('PT' . random_int(0, 525601) . 'M'));
     return [
-        'start_date' => $dateTime->add(new DateInterval('PT' . random_int(0, 525600) . 'M')), // 525600 = Sum of minutes of a year
-        'end_date' => $dateTime->add(new DateInterval('PT' . random_int(0, 525600) . 'M')),
+        'start_date' => $startDate,
+        'end_date' => $endDate,
         'second_name' => $faker->sentence,
         'project_id' => factory(App\Project::class)->make()->id,
         'location_id' => factory(App\Location::class)->make()->id,
