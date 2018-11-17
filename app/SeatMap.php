349cd0a6e5ead7cb665d96c08b2dd26d655ad9fd
Martin Gegenleitner <martin@gegenleitner.eu>
2018-11-17 14:02:38 +0100

    Added SeatMap-Model

---
+++ app/SeatMap.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SeatMap extends Model
+{
+    protected $fillable = ['rows', 'columns', 'name', 'description'];
+}
---
+++ database/migrations/2018_11_17_125910_create_seat_maps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeatMapsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seat_maps', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rows');
+            $table->integer('columns');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seat_maps');
+    }
+}
