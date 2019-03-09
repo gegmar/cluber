09e0b64b3b57399089fd2a878feaa341097a0743
Martin Gegenleitner <martin@gegenleitner.eu>
2019-03-09 09:45:43 +0100

    remove blank

--- config/mail.php
+++ config/mail.php
@@ -99,7 +99,7 @@ return [
     |
     */
 
-    'sendmail' => '/usr/sbin/sendmail -t -i ',
+    'sendmail' => '/usr/sbin/sendmail -t -i',
 
     /*
     |--------------------------------------------------------------------------
