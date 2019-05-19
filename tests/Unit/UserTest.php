454850b79ef81e8de2d5f13b247eb6cfc5e85897
Martin Gegenleitner <martin@gegenleitner.eu>
2019-05-19 14:09:23 +0200

    user model unit test added

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Role;
+use App\Permission;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testhasPermission()
+    {
+        $user = factory(User::class)->create();
+
+        $role1 = factory(Role::class)->create();
+        $pem1 = factory(Permission::class)->create();
+        $pem2 = factory(Permission::class)->create();
+
+        $role2 = factory(Role::class)->create();
+        $pem3 = factory(Permission::class)->create();
+
+        $role1->permissions()->attach([$pem1->id, $pem2->id]);
+        $role2->permissions()->attach($pem3);
+
+        // Test non existing permission
+        $this->assertFalse($user->hasPermission('FANTASY_PERMISSION'));
+        // Test permission not connected
+        $this->assertFalse($user->hasPermission($pem1->name));
+
+        $user->roles()->attach($role1->id);
+        $user->refresh();
+        // Test permission freshly connected
+        $this->assertTrue($user->hasPermission($pem1->name));
+
+        $user->roles()->attach($role2->id);
+        $user->refresh();
+        // Test old permission connected
+        $this->assertTrue($user->hasPermission($pem2->name));
+        // Test permission freshly connected
+        $this->assertTrue($user->hasPermission($pem3->name));
+    }
+}
