d55184ea7d44f1eab4c61d2b144755ad9e57588e
Martin Gegenleitner <martin@gegenleitner.eu>
2019-05-19 16:06:23 +0200

    Added Tests for TicketsController

---
+++ tests/Feature/TicketsControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Ticket;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Purchase;
+use App\Event;
+
+class TicketsControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testShowTicket()
+    {
+        $ticket = factory(Ticket::class)->create();
+        $response = $this->get("/ticket/$ticket->random_id");
+
+        $response->assertStatus(200);
+        $response->assertSee($ticket->purchase->customer->name);
+    }
+
+    public function testShowAndDownloadPurchase()
+    {
+        $purchase = factory(Purchase::class)->create();
+        $event = factory(Event::class)->create();
+        for ($i = random_int(0, 10); $i < 11; $i++) {
+            factory(Ticket::class)->create([
+                'purchase_id' => $purchase->id,
+                'event_id' => $event->id
+            ]);
+        }
+
+        $soldStates = ['paid', 'free', 'reserved'];
+
+        $response = $this->get("/ticket/$purchase->random_id/all");
+        $response->assertRedirect('/ts');
+
+        $response = $this->get("/ticket/$purchase->random_id/download");
+        $response->assertRedirect('/ts');
+
+        foreach ($soldStates as $state) {
+            $purchase->state = $state;
+            $purchase->save();
+            $response = $this->get("/ticket/$purchase->random_id/all");
+            $response->assertStatus(200);
+            $response->assertSee($purchase->total());
+        }
+    }
+}
